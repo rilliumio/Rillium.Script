8814b7137f9d5cc51b117bfbc38bb53a954f9647
rilliumio <>
2026-02-18 09:28:34 -0500

    tests

--- Rillium.Script.Test/CompiledScriptTests.cs
+++ Rillium.Script.Test/CompiledScriptTests.cs
@@ -77,5 +77,184 @@ namespace Rillium.Script.Test
             Assert.AreEqual(expected: "foo", script.Evaluate<string>("foo"));
             Assert.AreEqual(expected: "bar", script.Evaluate<string>("bar"));
         }
+
+        [TestMethod]
+        public void EvaluateDoubleResult()
+        {
+            CompiledScript script = Evaluator.Compile("var x = 3.14; x;");
+
+            Assert.AreEqual(expected: 3.14, script.Evaluate<double>());
+        }
+
+        [TestMethod]
+        public void EvaluateDoubleToIntConversion()
+        {
+            CompiledScript script = Evaluator.Compile("var x = 7.9; x;");
+
+            Assert.AreEqual(expected: 8, script.Evaluate<int>());
+        }
+
+        [TestMethod]
+        public void EvaluateDoubleToLongConversion()
+        {
+            CompiledScript script = Evaluator.Compile("var x = 100; x;");
+
+            Assert.AreEqual(expected: 100L, script.Evaluate<long>());
+        }
+
+        [TestMethod]
+        public void EvaluateDoubleToDecimalConversion()
+        {
+            CompiledScript script = Evaluator.Compile("var x = 5.5; x;");
+
+            Assert.AreEqual(expected: 5.5m, script.Evaluate<decimal>());
+        }
+
+        [TestMethod]
+        public void EvaluateBoolResult()
+        {
+            CompiledScript script = Evaluator.Compile("true;");
+
+            Assert.AreEqual(expected: true, script.Evaluate<bool>());
+        }
+
+        [TestMethod]
+        public void EvaluateStringResult()
+        {
+            CompiledScript script = Evaluator.Compile("'hello';");
+
+            Assert.AreEqual(expected: "hello", script.Evaluate<string>());
+        }
+
+        [TestMethod]
+        public void EvaluateByteArrayResult()
+        {
+            CompiledScript script = Evaluator.Compile("var x = [1, 2, 3]; x;");
+
+            CollectionAssert.AreEquivalent(
+                expected: new byte[] { 1, 2, 3 },
+                script.Evaluate<byte[]>());
+        }
+
+        [TestMethod]
+        public void EvaluateShortArrayResult()
+        {
+            CompiledScript script = Evaluator.Compile("var x = [10, 20, 30]; x;");
+
+            CollectionAssert.AreEquivalent(
+                expected: new short[] { 10, 20, 30 },
+                script.Evaluate<short[]>());
+        }
+
+        [TestMethod]
+        public void EvaluateIntArrayResult()
+        {
+            CompiledScript script = Evaluator.Compile("var x = [100, 200, 300]; x;");
+
+            CollectionAssert.AreEquivalent(
+                expected: new int[] { 100, 200, 300 },
+                script.Evaluate<int[]>());
+        }
+
+        [TestMethod]
+        public void EvaluateDoubleArrayResult()
+        {
+            CompiledScript script = Evaluator.Compile("var x = [1.1, 2.2, 3.3]; x;");
+
+            CollectionAssert.AreEquivalent(
+                expected: new double[] { 1.1, 2.2, 3.3 },
+                script.Evaluate<double[]>());
+        }
+
+        [TestMethod]
+        public void EvaluateNullResultThrows()
+        {
+            CompiledScript script = Evaluator.Compile("");
+
+            Assert.ThrowsException<ArgumentException>(
+                () => script.Evaluate<int>());
+        }
+
+        [TestMethod]
+        public void RunWithStreamWriter()
+        {
+            CompiledScript script = Evaluator.Compile("var x = 42; x;");
+
+            using var memoryStream = new MemoryStream();
+            using var streamWriter = new StreamWriter(memoryStream);
+
+            object? result = script.Run(streamWriter);
+
+            Assert.AreEqual(expected: 42.0, result);
+        }
+
+        [TestMethod]
+        public void RunWithStreamWriterAndArgs()
+        {
+            CompiledScript script = Evaluator.Compile("args[0];");
+
+            using var memoryStream = new MemoryStream();
+            using var streamWriter = new StreamWriter(memoryStream);
+
+            object? result = script.Run(streamWriter, 99);
+
+            Assert.AreEqual(expected: 99.0, result);
+        }
+
+        [TestMethod]
+        public void RunReturnsNullForEmptyScript()
+        {
+            CompiledScript script = Evaluator.Compile("");
+
+            var (output, console) = script.Run();
+
+            Assert.IsNull(output);
+            Assert.AreEqual(expected: "", console);
+        }
+
+        [TestMethod]
+        public void ReturnFromInsideIfBlock()
+        {
+            CompiledScript script = Evaluator.Compile(@"
+                var x = args[0];
+                if (x > 10) { return x * 2; }
+                return x;
+            ");
+
+            Assert.AreEqual(expected: 40, script.Evaluate<int>(20));
+            Assert.AreEqual(expected: 5, script.Evaluate<int>(5));
+        }
+
+        [TestMethod]
+        public void ReturnFromInsideForLoop()
+        {
+            CompiledScript script = Evaluator.Compile(@"
+                for (var i = 0; i < args[0]; i++) {
+                    if (i == 3) { return i; }
+                }
+                return -1;
+            ");
+
+            Assert.AreEqual(expected: 3, script.Evaluate<int>(10));
+            Assert.AreEqual(expected: -1, script.Evaluate<int>(2));
+        }
+
+        [TestMethod]
+        public void EvaluateWithNullArgs()
+        {
+            CompiledScript script = Evaluator.Compile("var x = 5; x;");
+
+            Assert.AreEqual(expected: 5, script.Evaluate<int>(null));
+        }
+
+        [TestMethod]
+        public void RunWithNullArgs()
+        {
+            CompiledScript script = Evaluator.Compile("var x = 10; x;");
+
+            var (output, console) = script.Run((object[]?)null);
+
+            Assert.AreEqual(expected: 10.0, output);
+        }
     }
 }
--- Rillium.Script.Test/StatementTests/DeclarationStatementTests.cs
+++ Rillium.Script.Test/StatementTests/DeclarationStatementTests.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rillium.Script.Exceptions;
+using Rillium.Script.Expressions;
 using Rillium.Script.Statements;
 
 namespace Rillium.Script.Test.StatementTests
@@ -14,6 +15,80 @@ namespace Rillium.Script.Test.StatementTests
             var ex = new ReturnStatement(token.BuildLiteralExpression());
 
             Assert.ThrowsException<ReturnStatementException>(() => ex.Execute(null));
+        }
+
+        [TestMethod]
+        public void ExecuteSetsVariableInScope()
+        {
+            var scope = new Scope(new FunctionTable());
+            var identifier = new Token(TokenId.Identifier, "myVar", 0);
+            var initializer = new NumberExpression(identifier, 42.0);
+            var statement = new DeclarationStatement(identifier, initializer);
+
+            statement.Execute(scope);
+
+            Assert.IsTrue(scope.HasVariable("myVar"));
+            Assert.IsTrue(scope.TryGet("myVar", out object? value));
+            Assert.IsInstanceOfType(value, typeof(NumberExpression));
+            Assert.AreEqual(expected: 42.0, ((NumberExpression)value).Value);
+        }
+
+        [TestMethod]
+        public void ExecuteWithNullInitializerSetsNull()
+        {
+            var scope = new Scope(new FunctionTable());
+            var identifier = new Token(TokenId.Identifier, "x", 0);
+            var statement = new DeclarationStatement(identifier, null);
+
+            statement.Execute(scope);
+
+            Assert.IsTrue(scope.HasVariable("x"));
+            Assert.IsTrue(scope.TryGet("x", out object? value));
+            Assert.IsNull(value);
+        }
+
+        [TestMethod]
+        public void ExecuteWithLiteralInitializer()
+        {
+            var scope = new Scope(new FunctionTable());
+            var identifier = new Token(TokenId.Identifier, "name", 0);
+            var literalToken = new Token(TokenId.String, "hello", 0);
+            var initializer = literalToken.BuildLiteralExpression();
+            var statement = new DeclarationStatement(identifier, initializer);
+
+            statement.Execute(scope);
+
+            Assert.IsTrue(scope.HasVariable("name"));
+            Assert.IsTrue(scope.TryGet("name", out object? value));
+            Assert.IsInstanceOfType(value, typeof(LiteralExpression));
+        }
+
+        [TestMethod]
+        public void ExecuteOverwritesExistingVariable()
+        {
+            var scope = new Scope(new FunctionTable());
+            var identifier = new Token(TokenId.Identifier, "x", 0);
+
+            var first = new DeclarationStatement(identifier, new NumberExpression(identifier, 1.0));
+            first.Execute(scope);
+
+            var second = new DeclarationStatement(identifier, new NumberExpression(identifier, 99.0));
+            second.Execute(scope);
+
+            Assert.IsTrue(scope.TryGet("x", out object? value));
+            Assert.IsInstanceOfType(value, typeof(NumberExpression));
+            Assert.AreEqual(expected: 99.0, ((NumberExpression)value).Value);
+        }
+
+        [TestMethod]
+        public void ConstructorSetsProperties()
+        {
+            var identifier = new Token(TokenId.Identifier, "z", 0);
+            var initializer = new NumberExpression(identifier, 7.0);
+            var statement = new DeclarationStatement(identifier, initializer);
+
+            Assert.AreEqual(identifier, statement.Identifier);
+            Assert.AreEqual(initializer, statement.Initializer);
         }
     }
 }
